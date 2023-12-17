dcd38cef2ff5012f857d5c2b952a8164a3cd0cae
JP Sauve <jean-paul@grokology.ca>
2023-12-17 13:29:06 -0500

    initial commit

---
+++ src/main/kotlin/ca/untrivial/routes/UserRoutes.kt
@@ -0,0 +1,20 @@
+package ca.untrivial.routes
+
+import io.ktor.server.routing.*
+
+fun Route.userRouting() {
+    route("/user") {
+        get {
+
+        }
+        get("{id?}") {
+
+        }
+        post {
+
+        }
+        delete("{id?}") {
+            
+        }
+    }
+}
