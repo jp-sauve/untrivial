428a52281d59e2cdf85ee15b29d058f528ff03b2
JP Sauve <jean-paul@grokology.ca>
2023-12-28 23:40:26 -0500

    update game model for exposed

--- src/main/kotlin/ca/untrivial/models/Game.kt
+++ src/main/kotlin/ca/untrivial/models/Game.kt
@@ -1,15 +1,18 @@
 package ca.untrivial.models
 
-import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.*
+import java.io.Serializable
 
-@Serializable
-data class Game(val id: String, val rounds: List<Round>)
 
-@Serializable
-data class Round(val id: String, val questions: List<Question>)
+data class Game(val id: String, val rounds: List<Round>): Serializable
 
-@Serializable
-data class Question(val id: String, val prompt: String,val correctChoice: Choice, val incorrectChoices: List<Choice>)
+data class Round(val id: String, val questions: List<Question>): Serializable
 
-@Serializable
-data class Choice(val id: String, val isCorrect: Boolean, val choiceText: String)
+data class Question(val id: String, val prompt: String,val correctChoice: Choice, val incorrectChoices: List<Choice>): Serializable
+
+data class Choice(val id: String, val isCorrect: Boolean, val choiceText: String): Serializable
+
+object Games: Table() {
+    val id = integer("id").autoIncrement()
+    val rounds = varchar("rounds", 128)
+}
