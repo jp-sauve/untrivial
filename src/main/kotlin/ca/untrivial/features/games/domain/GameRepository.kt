2daf5dc7ec0d33b763105ff6bdf2ee309032e724
JP Sauve <jean-paul@grokology.ca>
2023-12-31 12:36:35 -0500

    updates game.add function to return new game UUID

--- src/main/kotlin/ca/untrivial/features/games/domain/GameRepository.kt
+++ src/main/kotlin/ca/untrivial/features/games/domain/GameRepository.kt
@@ -2,9 +2,7 @@ package ca.untrivial.features.games.domain
 
 import ca.untrivial.dao.DatabaseSingleton.dbQuery
 import org.jetbrains.exposed.dao.id.UUIDTable
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import java.util.*
 
 
@@ -14,28 +12,26 @@ object Games : UUIDTable() {
 }
 
 class GameRepository {
-    suspend fun getAllGames(): List<GameDTO> = dbQuery {
+    suspend fun allGames(): List<GameDTO> = dbQuery {
         Games.selectAll().map {
             GameDTO(
-                id = it[Games.id].toString(),
-                name = it[Games.name],
-                variant = it[Games.variant]
+                id = it[Games.id].toString(), name = it[Games.name], variant = it[Games.variant]
             )
         }
     }
 
-suspend fun game(id: UUID): GameDTO? = dbQuery {
-    Games.select { Games.id eq id }.map {
-        GameDTO(
-            id = it[Games.id].toString(),
-            name = it[Games.name],
-            variant = it[Games.variant]
-        )
-    }.firstOrNull()
-
-}
-//    private fun toGame(row: ResultRow) = Game(
-//        name = row[Games.name],
-//        variant = row[Games.variant]
-//    )
+    suspend fun game(id: UUID): GameDTO? = dbQuery {
+        Games.select { Games.id eq id }.map {
+            GameDTO(
+                id = it[Games.id].toString(), name = it[Games.name], variant = it[Games.variant]
+            )
+        }.firstOrNull()
+    }
+    suspend fun add(dto: GameDTO): UUID = dbQuery {
+        val gameId = Games.insertAndGetId {
+            it[Games.name] = dto.name
+            it[Games.variant] = dto.variant
+        }
+        gameId.value
+    }
 }
