66a162232270e3d6f91a2a22c60d2705621dd72e
JP Sauve <jean-paul@grokology.ca>
2024-01-23 21:22:32 -0500

    start updating user routes to use service and repository instead of fake storage

--- src/main/kotlin/ca/untrivial/routes/UserRoutes.kt
+++ src/main/kotlin/ca/untrivial/routes/UserRoutes.kt
@@ -1,5 +1,7 @@
 package ca.untrivial.routes
 
+import ca.untrivial.features.users.domain.UserDTO
+import ca.untrivial.features.users.domain.UserService
 import ca.untrivial.models.User
 import ca.untrivial.models.userStorage
 import io.ktor.http.*
@@ -8,19 +10,26 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 
-fun Route.userRouting() {
+fun Route.userRouting(userService: UserService) {
+    val truth: Boolean = true
     route("/user") {
         get {
-            if (userStorage.isNotEmpty()) {
-                call.respond(userStorage)
+            if (truth) {
+                call.respond(userService.getAllUsers())
             } else {
                 call.respondText("Nothing to see", status = HttpStatusCode.NoContent)
             }
         }
         get("{id?}") {
-            val id = call.parameters["id"] ?: return@get call.respondText("Missing ID!", status = HttpStatusCode.BadRequest)
-            val customer = userStorage.find { user -> user.id == id } ?: return@get call.respondText("No user with id $id", status = HttpStatusCode.NotFound)
-            call.respond(customer)
+            val id = call.parameters["id"]?.toInt() ?: return@get call.respondText("Missing ID!", status = HttpStatusCode.BadRequest)
+//            val customer = userStorage.find { user -> user.id == id } ?: return@get call.respondText("No user with id $id", status = HttpStatusCode.NotFound)
+            val customer = userService.getUser(id)
+            if (customer != null) {
+                call.respond<UserDTO>(customer)
+            } else {
+                call.respondText(text = "Nothing to see for {}", contentType = ContentType.Text.Plain)
+            }
+
         }
         post {
             val user = call.receive<User>()
